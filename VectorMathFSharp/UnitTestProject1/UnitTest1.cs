e0f1f39f124037f30c8c635df2ebf2fc95f48dd5
Erich Ess <erich.g.ess@gmail.com>
2013-04-25 02:08:20 -0700

    Initial Check in

---
+++ VectorMathFSharp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class WhenAPointIsAddedWithAPoint
+    {
+        [TestMethod]
+        public void AndTheDimsDoNotMatch_Then_AnExceptionIsThrown()
+        {
+        }
+    }
+}
