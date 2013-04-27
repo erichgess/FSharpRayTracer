2c154f9a354b21b75bee6a62067846d90239e040
Erich Ess <erich.g.ess@gmail.com>
2013-04-27 10:24:22 -0700

    Deleted the Matrix4 class, because it's been supplanted by the more general Matrix class.

--- VectorMathFSharp/VectorMathFSharp/Matrix.fs
+++ VectorMathFSharp/VectorMathFSharp/Matrix.fs
@@ -95,65 +95,9 @@
         member this.Print () =
             sprintf "%A" xs
 
-    type Matrix4 (xs: float [,]) =
-        let xs = xs
-
-        member this.Item(i, j) = xs.[i, j]
-
-        member this.Transpose () =
-            Matrix4.init ( fun i j -> this.[ j, i ])
-
-        static member init f = Matrix4(Array2D.init 4 4 f)
-
-        static member RotateX (angle: float) =
-            let angle = angle / 180.0 * Math.PI
-            Matrix4( array2D[   [1.0; 0.0;            0.0;              0.0];
-                                [0.0; Math.Cos angle; -Math.Sin angle;  0.0];
-                                [0.0; Math.Sin angle; Math.Cos angle;   0.0];
-                                [0.0; 0.0;            0.0;              1.0]] )
-
-        static member RotateY (angle: float) =
-            let angle = angle / 180.0 * Math.PI
-            Matrix4( array2D[   [Math.Cos angle;  0.0; Math.Sin angle;  0.0];
-                                [0.0;             1.0; 0.0;             0.0];
-                                [-Math.Sin angle; 0.0; Math.Cos angle;  0.0];
-                                [0.0; 0.0;            0.0;              1.0]] )
-
-        static member RotateZ (angle: float) =
-            let angle = angle / 180.0 * Math.PI
-            Matrix4( array2D[   [Math.Cos angle; -Math.Sin angle; 0.0;  0.0];
-                                [Math.Sin angle; Math.Cos angle;  0.0;  0.0];
-                                [0.0;            0.0;             1.0;  0.0];
-                                [0.0; 0.0;            0.0;              1.0]] )
-
-        static member Scale (x: float, y: float, z: float ) =
-            Matrix4( array2D [  [  x; 0.0; 0.0; 0.0];
-                                [0.0;   y; 0.0; 0.0];
-                                [0.0; 0.0; z;   0.0];
-                                [0.0; 0.0; 0.0; 1.0] ] )
-
-        static member (*) (m: Matrix4, v: Vector4 ) =
-            Vector4.init (fun i -> v.X*m.[i,0] + v.Y*m.[i,1] + v.Z*m.[i,2] + v.W*m.[i,3])
-
-        static member (*) ( v: Vector4, m: Matrix4 ) =
-            Vector4.init (fun i -> v.X*m.[0,i] + v.Y*m.[1,i] + v.Z*m.[2,i] + v.W*m.[3,i])
-
-        static member (*) (m: Matrix4, p: Point4 ) =
-            Point4.init (fun i -> p.X*m.[i,0] + p.Y*m.[i,1] + p.Z*m.[i,2] + p.W*m.[i,3])
-
-        static member (*) (m: Matrix4, r: Ray ) =
-            Ray( m * r.Origin, m * r.Direction )
-            
-        static member (*) (m: Matrix4, n: Matrix4 ) =
-            Matrix4.init ( fun i j -> 
-            sum 0 3 (fun k -> m.[i, k] * n.[k, j]))
-
-        member this.Print () =
-            sprintf "%A" xs
-
 
     [<EntryPoint>]
     let main argv = 
-        let testM = Matrix4.init ( fun i j -> float(i*10 + j) )
+        let testM = Matrix.init 4 ( fun i j -> float(i*10 + j) )
         testM.Print ()
         0 // return an integer exit code
