4ce4caa435847f1223db7b2facd7425900362b0e
Erich Ess <erich.g.ess@gmail.com>
2013-05-02 08:23:20 -0700

    Added a timer to measure how long this takes to run.

--- VectorMathFSharp/VectorMathFSharp/Program.fs
+++ VectorMathFSharp/VectorMathFSharp/Program.fs
@@ -6,10 +6,12 @@ open Shape
 open Sphere
 open Plane
 open System.Drawing
+open System.Threading
+open System.Timers
 
 
-let xResolution = 256
-let yResolution = 256
+let xResolution = 32
+let yResolution = 32
 
 let GetCameraRay (u: int) (v: int ) =
     let center = Vector3( 0., 0., -8. )
@@ -35,7 +37,7 @@ let main argv =
     let SomeColor a = 
         Color.FromArgb(255, int( a * 200. ), 0, 0 )
 
-    let scene = [ new Plane( t) :> IShape; new Sphere( Matrix.Translate(0., 1., 0. ) ) :> IShape]
+    let scene = [  new Sphere( Matrix.Translate(0., 1., 0. ) ) :> IShape; new Plane( t) :> IShape]
     let CastRay x y = 
         let ray = GetCameraRay x y
         let intersections = scene |> List.map( fun s -> s.Intersection ray )
@@ -47,18 +49,23 @@ let main argv =
                 | None -> Some(time, normal)
                 | Some(accTime, accNormal) when time < accTime -> Some(time, normal)
                 | _ -> acc
-            )
-                            
+            )       
+    
         match nearestShape with
         | None -> Color.Black
-        | Some(p, n) -> 
+        | Some(time, n) -> 
+            let p = ray.Origin + ray.Direction * time
             let diffuse = n.Normalize() * ( Vector3( 0. - p.X, 7. - p.Y, -5. - p.Z ) ) .Normalize()
             let diffuse = if diffuse < 0. then 0. else diffuse
             SomeColor diffuse
     
+    let startTime = System.DateTime.Now
     for y= 0 to yResolution-1 do
-        for x = 0 to xResolution-1 do
+        for x = 0 to xResolution-1 do 
             bmp.SetPixel( x, y, (CastRay x y))
 
+    let endTime = System.DateTime.Now
+    let duration = (endTime - startTime).TotalSeconds
+    printfn "Duration: %f" duration
     bmp.Save("test.bmp" )
     0 // return an integer exit code
