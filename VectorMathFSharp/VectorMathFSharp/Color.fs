6ab4656bbe3163586f7434de58338c3a3d738e14
Erich Ess <erich.g.ess@gmail.com>
2013-05-08 13:56:23 -0700

    Added a new class to specifically handle color calculations (addition, scaling, etc.)

---
+++ VectorMathFSharp/VectorMathFSharp/Color.fs
@@ -0,0 +1,45 @@
+﻿module Color
+    type BoundedMath () =
+            member this.Bind(input, rest ) =
+                match input with
+                | input when input < 0. -> rest 0.
+                | input when input > 1. -> rest 1.
+                | _ -> rest input 
+
+            member this.Return(x) =
+                x
+
+    type Color( r: float, g: float, b: float ) =
+        member this.R = r
+        member this.G = g
+        member this.B = b
+
+        static member (*) ( c: Color, d: Color ) =
+            let calculateColor = new BoundedMath()
+            calculateColor{
+               let! r = c.R * d.R
+               let! g = c.G * d.G
+               let! b = c.B * d.B
+               return Color( r, g, b )
+            }
+
+        static member (+) ( c: Color, d: Color ) =
+            let calculateColor = new BoundedMath()
+            calculateColor{
+               let! r = c.R + d.R
+               let! g = c.G + d.G
+               let! b = c.B + d.B
+               return Color( r, g, b )
+            }
+
+        static member (*) ( a: float, c: Color ) =
+            let calculateColor = new BoundedMath()
+            calculateColor{
+               let! r = a * c.R
+               let! g = a * c.G
+               let! b = a * c.B
+               return Color( r, g, b )
+            }
+
+        static member (*) ( c: Color, a: float ) =
+            a * c
