4e791263f66408e220d8ac5487dffad302ca6393
Erich Ess <erich.g.ess@gmail.com>
2013-05-17 08:49:05 -0700

    Added a photon mapper, which takes a light and a scene and randomly traces photons through the scene.  Building a KD Tree of all the photons.

---
+++ VectorMathFSharp/VectorMathFSharp/PhotonMapper.fs
@@ -0,0 +1,42 @@
+﻿module PhotonMapper
+
+    open RayTracer
+    open Scene
+    open Light
+    open System
+    open Point
+    open Ray
+    open KDTree
+
+    let RandomPoint () =
+        let rand = new Random()
+        Point3( 2. * rand.NextDouble() - 1., 0., rand.NextDouble() * 2. - 1. )
+    
+    let CalculatePhotonMapForLight (scene: Scene) (light: Light ) =
+        // pick a random point in the unit circle
+        let rayTarget = 4. * RandomPoint()
+
+        // Create a ray which passes through that point
+        let ray = Ray( light.Position, (rayTarget - light.Position).Normalize() )
+
+        // Build an illumination tree for this ray
+        let illuminationTree = BuildLightRayTree scene 5 ray
+
+        // Traverse the illumination tree and add each point to a list
+        let rec BuildPointList  (tree: IlluminationTree) =
+            match tree with
+            | NoIllumination -> []
+            | IlluminationSource(hit, reflected, refracted ) -> (hit.Point, hit.Illumination) :: BuildPointList refracted
+        
+        BuildPointList illuminationTree
+
+    let BuildListOfPhotons (scene: Scene) (light: Light) =
+        let mutable photonList = []
+
+        for i = 1 to 1000 do
+            photonList <- photonList @ (CalculatePhotonMapForLight scene light )
+        photonList
+
+    let BuildPhotonMap (scene: Scene) (light: Light ) =
+        let photonList = BuildListOfPhotons scene light
+        BuildKdTree photonList 0
