e4bf6ef06e6589bf1c6f7508343cbbb6229a5941
Erich Ess <erich.g.ess@gmail.com>
2013-05-19 11:47:49 -0700

    FindAllPointsNearPoint2 now only returns the color, rather than the point and color.  The point was not being used for anything.
    Also fixed a bug where FindAllPointsNearPoint2 was calling FindAllPointsNearPoint.

--- VectorMathFSharp/VectorMathFSharp/KDTree.fs
+++ VectorMathFSharp/VectorMathFSharp/KDTree.fs
@@ -17,7 +17,7 @@
             let axis = (depth) % 3
         
             let length = List.length photonList
-            let median = (List.length photonList) / 2
+            let median = length / 2
             let sortedPhotonArray = photonList |> List.sortBy ( fun (p,c) -> p.[axis] ) |> List.toArray
 
             // Add an index to each element of the list so that we can easily partition
@@ -58,16 +58,20 @@
     let rec FindAllPointsNearPoint2 (tree: KDTreeNode<'a>) (target: Point3 ) (radiusSquared: float) (depth: int)=
         match tree with
         | Empty -> []
+        | Node( Empty, point, data, Empty ) -> if (point-target).Length() <= radiusSquared then [data] else []
         | Node( left, point, data, right ) -> 
             let axis = (depth) % 3
-            let points =    if System.Math.Abs ( target.[axis] - point.[axis] ) > radiusSquared then
-                                (FindAllPointsNearPoint left target radiusSquared (depth+1)) @ (FindAllPointsNearPoint right target radiusSquared (depth+1))
-                            else if target.[axis] < point.[axis] then
-                                FindAllPointsNearPoint left target radiusSquared (depth+1)
+            let t1 = target.[axis]
+            let p1 = point.[axis]
+            let points =    if System.Math.Abs ( t1 - p1 ) <= radiusSquared then
+                                (FindAllPointsNearPoint2 left target radiusSquared (depth+1)) 
+                                @ (FindAllPointsNearPoint2 right target radiusSquared (depth+1))
+                            else if t1 < p1 then
+                                FindAllPointsNearPoint2 left target radiusSquared (depth+1)
                             else
-                                FindAllPointsNearPoint right target radiusSquared (depth+1)
+                                FindAllPointsNearPoint2 right target radiusSquared (depth+1)
             let x = point - target
             if x.Length() <= radiusSquared then
-                (point,data) :: points
+                data :: points
             else
                 points
